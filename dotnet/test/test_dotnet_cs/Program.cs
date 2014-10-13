a405e118698e2bee7322b33c14c882adb86c1226
Vincent Ycasas <vincent.ycasas@gmail.com>
2014-10-13 23:34:57 +0900

    [DotNet] Added Implementation.
    * Tests are done with C++/CLI.
    * C# and VB tests are not part of CMake build system.

---
+++ dotnet/test/test_dotnet_cs/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+using DotNetLib;
+
+namespace test_dotnet_cs
+{
+    class MyGenerator : IGenerator
+    {
+        public int GenerateInt(int data)
+        {
+            return (data * data);
+        }
+
+        public String GenerateString(int data)
+        {
+            return (String.Format("{0}", data));
+        }
+    }
+
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            try {
+                Library.Initialize();
+
+                Console.Out.WriteLine("Creating a new address...");
+                Address address = new Address(
+                    9898, "Corner St.",
+                    "Gotham City", "CA",
+                    "Antartica", "6758"
+                );
+                Console.Out.WriteLine("New address created!");
+                Console.Out.WriteLine("Address:");
+                Console.Out.WriteLine(address.ToString());
+
+                Console.Out.WriteLine("Creating a new person...");
+                Person person = new Person(
+                    "Wyane", "Bruce", 25, address
+                );
+                Console.Out.WriteLine("New person created!");
+                Console.Out.WriteLine("Person:");
+                Console.Out.WriteLine(person.ToString());
+
+                Console.Out.WriteLine("Creating a new generator...");
+                MyGenerator generator = new MyGenerator();
+                Console.Out.WriteLine("New generator created!");
+                Console.Out.WriteLine("Creating a new printer...");
+                Printer printer = new Printer(generator);
+                Console.Out.WriteLine("New printer created!");
+                Console.Out.WriteLine("Performing printer actions...");
+                printer.PrintInt();
+                printer.PrintString();
+
+                Library.Terminate();
+            }
+            catch (DotNetLib.Exception e) {
+                Console.Error.WriteLine("An error has occurred: {0}", e.Message);
+                return (-1);
+            }
+            catch (System.Exception e) {
+                Console.Error.WriteLine("An error has occurred: {0}", e.Message);
+                return (-1);
+            }
+            return (0);
+        }
+    }
+}
