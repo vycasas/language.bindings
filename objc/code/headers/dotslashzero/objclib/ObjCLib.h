a26a0a35dddd61c40f8b0cad05dbbaa2614fd185
Vincent Ycasas <vincent.ycasas+github@gmail.com>
2019-06-13 08:53:35 -0700

    Objective-C updated to use C interface directly
    This commit also fixes the callback functionalities.

--- objc/code/headers/dotslashzero/objclib/ObjCLib.h
+++ objc/code/headers/dotslashzero/objclib/ObjCLib.h
@@ -1,87 +1,84 @@
-#if !defined(OBJCLIB_API_H)
-#define OBJCLIB_API_H
-
 #import <Foundation/Foundation.h>
 
 #if defined(__cplusplus)
 extern "C"
 {
-#endif /* defined(__cplusplus) */ 
+#endif /* defined(__cplusplus) */
 
 @interface OLException : NSException
-- (void) dealloc;
-- (NSString*) getMessage;
 @end /* interface OLException */
 
 @interface OLLibrary : NSObject
+
 + (void) initialize;
-+ (void) terminate;
++ (void) uninitialize;
 + (NSString*) getVersionString;
 + (NSUInteger) getVersionMajor;
 + (NSUInteger) getVersionMinor;
++ (NSUInteger) getVersionPatch;
++ (NSString*) getVersionExtra;
+
 @end /* interface OLLibrary */
 
 @interface OLAddress : NSObject
 
-- (id) initWithStreetNum:(int)streetNum street:(NSString*)street
-    city:(NSString*)city province:(NSString*)province
-    country:(NSString*)country zipCode:(NSString*)zipCode;
+- (instancetype) initWithStreetNum: (int) streetNum
+    street: (NSString*) street
+    city: (NSString*) city
+    province: (NSString* )province
+    zipCode: (NSString*) zipCode
+    country: (NSString*) country;
 - (void) dealloc;
-- (NSString*) toString;
 
 @property (readonly) int streetNum;
 @property (readonly) NSString* street;
 @property (readonly) NSString* city;
 @property (readonly) NSString* province;
-@property (readonly) NSString* country;
 @property (readonly) NSString* zipCode;
+@property (readonly) NSString* country;
+
+- (NSString*) toString;
 
 @end /* interface OLAddress */
 
 @interface OLPerson : NSObject
 
-- (id) initWithLastName:(NSString*)lastName firstName:(NSString*)firstName
-    age:(int)age address:(OLAddress*)address;
+- (instancetype) initWithLastName: (NSString*) lastName
+    firstName: (NSString*) firstName
+    age: (int) age
+    address: (OLAddress*) address;
 - (void) dealloc;
-- (NSString*) toString;
 
 @property (readonly) NSString* lastName;
 @property (readonly) NSString* firstName;
 @property (readonly) int age;
 @property (readonly) OLAddress* address;
 
+- (NSString*) toString;
+
 @end /* interface OLPerson */
 
-/*
-** There is an issue with protocols when transferring ownership. I have to investigate this issue further. In the mean
-** time, a normal interface works (and throwing exceptions on non-overriden required methods).
-** In particular, there is an issue when retaining and transfering ownership to and from ARC system.
 @protocol OLGenerator <NSObject>
 
 @required
 - (void) dealloc;
-- (int) generateIntWithData:(int)data;
-- (NSString*) generateStringWithData:(int)data;
 
-@end *//* protocol OLGenerator */
+- (int) generateIntWithData: (int) data;
+- (NSString*) generateStringWithData: (int) data;
 
-@interface OLGenerator : NSObject
-- (void) dealloc;
-- (int) generateIntWithData:(int)data;
-- (NSString*) generateStringWithData:(int)data;
-@end /* interface OLGenerator */
+@end /* protocol OLGenerator */
 
 @interface OLPrinter : NSObject
 
-- (id) initWithGenerator:(OLGenerator*)generator;
+- (instancetype) initWithGenerator: (id<OLGenerator>) generator;
 - (void) dealloc;
+
 - (void) printInt;
 - (void) printString;
 
 @end /* interface OLPrinter */
 
 #if defined(__cplusplus)
-} /* extern "C" */
+}
+/* extern "C" */
 #endif /* defined(__cplusplus) */
-
-#endif /* !defined(OBJCLIB_API_H) */
